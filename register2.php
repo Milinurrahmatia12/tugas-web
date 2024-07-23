52223547b65f80bf29dae286ff8344167a5a260c
Milinurrahmatia12 <milinurrahmatia120304@gmail.com>
2024-07-23 22:03:42 +0800

    first github

---
+++ register2.php
@@ -0,0 +1,15 @@
+<?php
+require 'koneksi.php';
+$username = $_POST["username"];
+$institution = $_POST["institution"];
+$email = $_POST["email"];
+$password = $_POST["password"];
+
+$query_sql = "INSERT INTO users (username, institution, email, password) 
+            VALUES ('$username', '$institution', '$email', '$password')";
+
+if (mysqli_query($conn, $query_sql)) {
+    header("Location: index.html");
+} else {
+    echo "Pendaftaran Gagal : " . mysqli_error($conn);
+}
